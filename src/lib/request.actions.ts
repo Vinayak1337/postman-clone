4df9523f02ae5a641a207d9bbf2dc415cdb538dd
Vinayak1337 <34209962+Vinayak1337@users.noreply.github.com>
2024-01-02 03:33:20 +0530

    fixed base_url issue

--- src/lib/request.actions.ts
+++ src/lib/request.actions.ts
@@ -1,7 +1,10 @@
 import { Prisma } from '@prisma/client';
 import axios from 'axios';
 
-const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? process.env.NEXT_PUBLIC_API_BASE_URL
+    : process.env.NEXT_PUBLIC_API_BASE_URL_PROD;
 
 export const fetchRequests = async () => {
   try {
